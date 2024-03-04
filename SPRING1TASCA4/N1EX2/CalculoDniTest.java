ff5643f0129720ae1b836664f2846eff4a968027
aperez1haad <159771117+aperez1haad@users.noreply.github.com>
2024-03-04 11:31:07 +0100

    Add files via upload

--- SPRING1TASCA4/N1EX2/CalculoDniTest.java
+++ SPRING1TASCA4/N1EX2/CalculoDniTest.java
@@ -1,50 +1,40 @@
 package INTELIJ.SPRING1TASCA4.N1EX2;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+import java.util.Collection;
+
+
+@RunWith(Parameterized.class)
 public class CalculoDniTest {
+    private int dni;
+    private char letraEsperada;
+
+    public CalculoDniTest(int dni, char letraEsperada) {
+        this.dni = dni;
+        this.letraEsperada = letraEsperada;
+    }
+    @Parameterized.Parameters
+    public static Collection<Object[]> DNISdePrueba() {
+        return Arrays.asList(new Object[][] {
+                { 12345678, 'Z' },
+                { 87654321, 'X' },
+                { 98765432, 'M' },
+                { 54321678, 'W' },
+                { 13579246, 'T' },
+                { 24681357, 'B' },
+                { 11111111, 'H' },
+                { 22222222, 'J' },
+                { 33333333, 'P' },
+                { 44444444, 'A' }
+        });
+    }
     @Test
     public void testCalcularLetraDni() {
-        char letraCalculada1 = CalculoDni.CalcularLetraDni(12345678);
-        assertEquals('Z', letraCalculada1);
-
-        char letraCalculada2 = CalculoDni.CalcularLetraDni(87654321);
-        assertEquals('X', letraCalculada2);
-
-        char letraCalculada3 = CalculoDni.CalcularLetraDni(98765432);
-        assertEquals('M', letraCalculada3);
-
-        char letraCalculada4 = CalculoDni.CalcularLetraDni(54321678);
-        assertEquals('W', letraCalculada4);
-
-        char letraCalculada5 = CalculoDni.CalcularLetraDni(13579246);
-        assertEquals('T', letraCalculada5);
-
-        char letraCalculada6 = CalculoDni.CalcularLetraDni(24681357);
-        assertEquals('B', letraCalculada6);
-
-        char letraCalculada7 = CalculoDni.CalcularLetraDni(11111111);
-        assertEquals('H', letraCalculada7);
-
-        char letraCalculada8 = CalculoDni.CalcularLetraDni(22222222);
-        assertEquals('J', letraCalculada8);
-
-        char letraCalculada9 = CalculoDni.CalcularLetraDni(33333333);
-        assertEquals('P', letraCalculada9);
-
-        char letraCalculada10 = CalculoDni.CalcularLetraDni(44444444);
-        assertEquals('A', letraCalculada10);
-
-
-        // UNA MANERA MÁS CORTA DE HACERLO
-
-        int[] numerosDNI = {12345678, 87654321, 98765432, 54321678, 13579246,
-                24681357, 11111111, 22222222, 33333333, 44444444};
-        char[] letrasEsperadas = {'Z', 'X', 'M', 'W', 'T', 'B', 'H', 'J', 'P', 'A'};
-
-        for (int i = 0; i < numerosDNI.length; i++) {
-            char letraCalculada = CalculoDni.CalcularLetraDni(numerosDNI[i]);
-            assertEquals(letrasEsperadas[i], letraCalculada);
-        }
+        char letraCalculada = CalculoDni.CalcularLetraDni(dni);
+        assertEquals(letraEsperada, letraCalculada);
     }
 }
 

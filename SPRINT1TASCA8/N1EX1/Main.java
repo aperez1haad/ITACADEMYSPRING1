eacd2a38835084ee5c754c518d44925c1f84c9d0
aperez1haad <159771117+aperez1haad@users.noreply.github.com>
2024-03-07 10:49:48 +0100

    Add files via upload

---
+++ SPRINT1TASCA8/N1EX1/Main.java
@@ -0,0 +1,27 @@
+package INTELIJ.SPRINT1TASCA8.N1EX1;
+import java.util.ArrayList;
+
+public class Main {
+    public static ArrayList<String> PalabrasconO (ArrayList<String>nombres){
+        ArrayList<String> resultado = new ArrayList<>();
+        nombres.forEach(s1 -> {
+            if(s1.contains("o")){
+                resultado.add(s1);
+            }
+        });
+        return resultado;
+    }
+    public static void main (String[] args){
+        ArrayList<String> nombres = new ArrayList<>();
+        nombres.add("Juan");
+        nombres.add("Marcos");
+        nombres.add("Alex");
+        nombres.add("Pol");
+        nombres.add("Anna");
+        nombres.add("Fanny");
+        nombres.add("Antonio");
+
+        ArrayList<String>nombresconO = PalabrasconO(nombres);
+        System.out.println("Palabras con 'O': "+nombresconO);
+    }
+}
---
+++ SPRINT1TASCA8/N1EX7/Main.java
@@ -0,0 +1,25 @@
+package INTELIJ.SPRINT1TASCA8.N1EX7;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class Main {
+    public static void main (String[] args) {
+        ArrayList<String> nombres = new ArrayList<>();
+        nombres.add("Juan354");
+        nombres.add("5464Marcos");
+        nombres.add("Alex567");
+        nombres.add("h653uhPolhnu687");
+        nombres.add("Annateyj566");
+        nombres.add("Fanny54hn765");
+        nombres.add("A4y4ntoniog5634");
+
+        nombres.sort(Comparator.comparingLong(String::length).reversed());
+        nombres.forEach(System.out::println);
+
+        //otra forma de hacer sin el comparator
+        nombres.sort((s1,s2)-> Integer.compare(s2.length(),s1.length()));
+        nombres.forEach(System.out::println);
+
+    }
+}

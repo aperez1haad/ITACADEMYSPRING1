85192c431156b078e392b5fa3be18306c8f59321
aperez1haad <159771117+aperez1haad@users.noreply.github.com>
2024-03-07 10:43:43 +0100

    Add files via upload

---
+++ SPRINT1TASCA2/src/N1EX1/Main.java
@@ -0,0 +1,26 @@
+package INTELIJ.SPRINT1TASCA2.src.N1EX1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Venda vendatrimestral = new Venda();
+        vendatrimestral.addProduct(new Producte("Pa", 1.5));
+        vendatrimestral.addProduct(new Producte("Llet", 2.0));
+        vendatrimestral.addProduct(new Producte("Oli", 150.0));
+        vendatrimestral.addProduct(new Producte("Ous", 20.0));
+
+        try {
+            vendatrimestral.calcularTotal();
+            System.out.println("Preu total: " + vendatrimestral.getPreuTotal());
+            // Provocar ArrayIndexOutOfBoundsException
+            // Accedint a un producte fora de l'índex
+            Producte producteInaccessible = vendatrimestral.productList.get(6);
+            System.out.println("Nom del producte: " + producteInaccessible.getNom());
+        } catch (VendaBuidaException e) {
+            System.out.println(e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Hi ha hagut un error: " + e.getMessage());
+        }
+    }
+
+}
---
+++ SPRINT1TASCA2/src/N1EX1/VendaBuidaException.java
@@ -0,0 +1,8 @@
+package INTELIJ.SPRINT1TASCA2.src.N1EX1;
+
+public class VendaBuidaException extends Exception{
+    public VendaBuidaException(String mensaje) {
+        super(mensaje);
+    }
+}
+

25ec9737b4f7aa67822588e6f5c578a283b199ef
aperez1haad <159771117+aperez1haad@users.noreply.github.com>
2024-03-07 10:52:46 +0100

    Delete SPRING1TASCA4 directory

--- SPRING1TASCA4/N1EX2/CalculoDni.java
+++
@@ -1,7 +0,0 @@
-package INTELIJ.SPRING1TASCA4.N1EX2;
-public class CalculoDni {
-    public static char CalcularLetraDni(int numeroDNI) {
-        String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
-        return letras.charAt(Math.round(numeroDNI % 23));
-    }
-}

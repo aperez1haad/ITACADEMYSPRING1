3bcb54e12fc971d1750b14ff55e3ab524c0e58b4
aperez1haad <159771117+aperez1haad@users.noreply.github.com>
2024-03-07 10:52:29 +0100

    Delete SPRING1TASCA2 directory

--- SPRING1TASCA2/src/N1EX1/VendaBuidaException.java
+++
@@ -1,8 +0,0 @@
-package INTELIJ.SPRING1TASCA2.src.N1EX1;
-
-public class VendaBuidaException extends Exception{
-    public VendaBuidaException(String mensaje) {
-        super(mensaje);
-    }
-}
-

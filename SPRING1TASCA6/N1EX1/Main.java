0b40a4162c470f2e7b7faddf8f0614764534bf23
aperez1haad <159771117+aperez1haad@users.noreply.github.com>
2024-03-07 10:22:27 +0100

    Add files via upload

--- SPRING1TASCA6/N1EX1/Main.java
+++ SPRING1TASCA6/N1EX1/Main.java
@@ -2,15 +2,15 @@ package INTELIJ.SPRING1TASCA6.N1EX1;
 
 public class Main {
     public static void main(String[] args) {
-        NoGenericMethods noGenericMethods1 = new NoGenericMethods("Primer objecte", "Segon objecte", "Tercer objecte");
-        NoGenericMethods noGenericMethods2 = new NoGenericMethods("Segon objecte", "Primer objecte",  "Tercer objecte");
-        NoGenericMethods noGenericMethods3 = new NoGenericMethods("Tercer objecte", "Segon objecte", "Primer objecte");
+        NoGenericMethods noGenericMethods1 = new NoGenericMethods("Primer String", "Segon String", "Tercer String");
+        NoGenericMethods noGenericMethods2 = new NoGenericMethods("Segon String", "Primer String",  "Tercer String");
+        NoGenericMethods noGenericMethods3 = new NoGenericMethods("Tercer String", "Segon String", "Primer String");
 
-        System.out.println(noGenericMethods1.extraer(1)); // Debería imprimir "Primer objecte"
-        System.out.println(noGenericMethods2.extraer(2)); // Debería imprimir "Primer objecte"
-        System.out.println(noGenericMethods3.extraer(3)); // Debería imprimir "Primer objecte"
+        System.out.println(noGenericMethods1.extraer(1)); // Debería imprimir "Primer String"
+        System.out.println(noGenericMethods2.extraer(2)); // Debería imprimir "Primer String"
+        System.out.println(noGenericMethods3.extraer(3)); // Debería imprimir "Primer String"
 
-        noGenericMethods1.modificarObjeto(1,"Primer objeto nuevo");
+        noGenericMethods1.modificarObjeto(1,"Primer String nuevo");
         System.out.println(noGenericMethods1.extraer(1));
 
     }
